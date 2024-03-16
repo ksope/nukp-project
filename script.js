a8cde4cf781bb7d10f8f7e73f041cc701ea20273
ksope <105855785+ksope@users.noreply.github.com>
2024-03-16 21:44:33 +0000

    add click events to toggle menu

---
+++ script.js
@@ -0,0 +1,10 @@
+const navLinks = document.getElementById("nav-links");
+
+//show menu function
+const showMenu = () => {
+    navLinks.style.right = "0";
+};
+
+const hideMenu = () => {
+    navLinks.style.right = "-200px";
+};
