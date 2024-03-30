dc9c4b913ee81c30092d7774fb7db267ca2516b6
ksope <105855785+ksope@users.noreply.github.com>
2024-03-30 10:42:21 +0000

    add about-us page with styling for desktop and mobile

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ const showMenu = () => {
 const hideMenu = () => {
     navLinks.style.right = "-200px";
 };
+
+//read more button
+const readMoreBtn = document.querySelector(".read-more");
+const readMoreContent = document.querySelector(".read-more-content");
+
+//show more pararaphs on clicking read more link
+readMoreBtn.addEventListener("click", () => {
+    readMoreContent.classList.toggle("show-content");
+    if (readMoreContent.classList.contains("show-content")) {
+        readMoreBtn.textContent = "Show less";
+    } else {
+        readMoreBtn.textContent = "Show more";
+    }
+});
